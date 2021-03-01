cdacf687bda2d9dc5806fd82a48fbcfa2b55ee6c
marcellothearcane <maxlreynolds@outlook.com>
2021-03-01 16:35:29 +0000

    Add jsx presets

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 const devPresets = ['@vue/babel-preset-app'];
-const buildPresets = ['@babel/preset-env', '@babel/preset-typescript'];
+const buildPresets = ['@babel/preset-env', '@babel/preset-typescript', '@vue/babel-preset-jsx'];
 module.exports = {
   presets: (process.env.NODE_ENV === 'development' ? devPresets : buildPresets),
+  // plugins: ['@babel/plugin-syntax-jsx'],
 };
